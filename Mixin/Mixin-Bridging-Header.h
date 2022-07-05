4db02f1070bb4c01fe4bfa59b55ea98947e00653
fanyu <fanyukobe@gmail.com>
2022-07-05 14:57:25 +0800

    Detect link by FastURLDetector

--- Mixin/Mixin-Bridging-Header.h
+++ Mixin/Mixin-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "MXMFastURLDetector.h"
+#import "MXSFastURLDetector.h"
 #import "MXMProvisionCryptor.h"
 #import "opusfile.h"
 #import "opusenc.h"
